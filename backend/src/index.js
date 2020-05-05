87d42dd55fd6c27c1f4ebac8711bd3883e052a46
Jair Batista Junior <jair.batista@adtsys.com.br>
2020-05-05 20:31:39 -0300

    Initial import

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const PORT = 3333;
+
+const server = express();
+
+server.get("/ping", (req, res) => {
+  return res.send("pong");
+})
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}.`);
+});
